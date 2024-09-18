2b365680cef3607128930fb0c3085eebde3fd4d4
Jungwoo Hong <ghost1434@inu.ac.kr>
2024-09-18 20:06:38 +0900

    feat: 컴포넌트 분할, entrypoint 변경

--- src/core/Component.js
+++ src/core/Component.js
@@ -1,13 +1,16 @@
 export default class Component {
   $target;
+  props;
   state;
   constructor($target) {
     this.$target = $target;
+    this.props = props;
     this.setup();
     this.setEvent();
     this.render();
   }
   setup() {}
+  mounted() {}
   template() {
     return "";
   }
